d3fa0b3aa6d0088f9270ebf3b0e4579f5956e22f
elijahcantu <caelijah@umich.edu>
2024-10-09 02:10:26 -0400

    fixed styling

--- js/content.js
+++ js/content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function (request, sender, response) {
     if (request.type === 'enable') {
-        document.querySelector('html').style.filter = "grayscale(100%)";
+        document.querySelector('html').style.setProperty('filter', 'grayscale(100%)', 'important');
     }
     if (request.type === 'disable') {
         document.querySelector('html').style.filter = "";
